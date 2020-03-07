e9fee8575a81f8ec9b812c11315bbe85adc35001
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-07 09:44:40 -0500

    display first item in a stack is working correctly

--- Stack.js
+++ Stack.js
@@ -1,24 +1,29 @@
 class _Node{
+    //data is the new node to be created next is what it points to 
     constructor(data,next){
     this.data=data;
-    this.next=null;
+    this.next=next;
     }
 
 }
 
 class Stack{
 constructor(){
+    //instantiates with a value of null 
     this.top=null
 }
 
 push(data){
+
 if(this.top===null){
 this.top = new _Node(data,null)
 return this.top
 }
+else{
 
 const node = new _Node(data,this.top);
 this.top = node;
+}
 } // end push function
 
 pop(){
--- practice1.js
+++ practice1.js
@@ -3,16 +3,38 @@
 
 // Create a stack called starTrek and add Kirk, Spock, McCoy, and Scotty to the stack.
 
-const Stack = require('./Stack')
+const Stack = require("./Stack");
 
+function peek(stack) {
+  console.log("stack top", stack.top);
+}
 
+const isEmpty = stack => {
+  if (stack.top == null) {
+    console.log(`stack is empty!!`);
+  } else {
+    console.log(`NO, stack is not empty`);
+  }
+};
+
+const displayFirst = stack => {
+  let first = stack.top;
+  let temp = stack.top;
+  while (temp.next != null) {
+    temp = temp.next;
+  }
+  console.log("this is the first item it the stack", temp);
+};
 const starTrek = new Stack();
-console.log("inside of startrek",starTrek)
-starTrek.push('Kirk')
-console.log("inside of startrek",starTrek)
-starTrek.push('Spock')
-console.log("inside of startrek",starTrek)
-starTrek.push('McCoy')
-console.log("inside of startrek",starTrek)
-starTrek.push('Scotty')
-console.log("inside of startrek",starTrek)
+console.log("inside of startrek", starTrek);
+starTrek.push("Kirk");
+console.log("inside of startrek", starTrek);
+starTrek.push("Spock");
+console.log("inside of startrek", starTrek);
+starTrek.push("McCoy");
+console.log("inside of startrek", starTrek);
+starTrek.push("Scotty");
+console.log("inside of startrek", starTrek);
+peek(starTrek);
+displayFirst(starTrek);
+isEmpty(starTrek);
